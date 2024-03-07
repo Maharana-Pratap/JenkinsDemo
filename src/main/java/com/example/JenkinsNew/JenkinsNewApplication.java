aef3f15cd543e5c63cad5ef470ecf0c94e8da3a0
pratap_51 <maharana1982@gmail.com>
2024-03-07 12:35:52 +0530

    fjile formation

--- src/main/java/com/example/JenkinsNew/JenkinsNewApplication.java
+++ src/main/java/com/example/JenkinsNew/JenkinsNewApplication.java
@@ -2,6 +2,7 @@ package com.example.JenkinsNew;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/jenkins")
 public class JenkinsNewApplication extends SpringBootServletInitializer {
+
+    @Override
+    public SpringApplicationBuilder
+    configure(SpringApplicationBuilder application) {
+        return application.sources(
+                JenkinsNewApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsNewApplication.class, args);
