21704e67bb8788bb23fda1165685ed9ecf9fe75d
pratap_51 <maharana1982@gmail.com>
2024-03-07 12:42:28 +0530

    code reversed

--- src/main/java/com/example/JenkinsNew/JenkinsNewApplication.java
+++ src/main/java/com/example/JenkinsNew/JenkinsNewApplication.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/*
-this is ca change for reversing code test
- */
 @SpringBootApplication
 @RestController
 @RequestMapping("/jenkins")
