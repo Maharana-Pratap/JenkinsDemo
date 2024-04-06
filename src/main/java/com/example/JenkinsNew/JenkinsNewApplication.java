7a7fc36c5ce11db71bd46f2700f44df243452654
Maharana <45598520+Maharana-Pratap@users.noreply.github.com>
2024-04-06 14:46:41 +0530

    tomcat war deployed code added

--- src/main/java/com/example/JenkinsNew/JenkinsNewApplication.java
+++ src/main/java/com/example/JenkinsNew/JenkinsNewApplication.java
@@ -1,7 +1,9 @@
 package com.example.JenkinsNew;
 
+import com.example.JenkinsNew.pojo.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,15 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/jenkins")
 public class JenkinsNewApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsNewApplication.class, args);
     }
 
-    @GetMapping("/test")
+    @Override
+    public SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JenkinsNewApplication.class);
+    }
+
+    @GetMapping("/")
     public String test() {
         return "I am from Jenkins Test Demo";
+    }
+
+    @GetMapping("/user")
+    public User getUser() {
+        return User.builder()
+                .userEmail("mps@email.com")
+                .userName("Maharana Pratap")
+                .userPhone("23453434")
+                .build();
     }
 }
